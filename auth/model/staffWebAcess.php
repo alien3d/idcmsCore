4ace73f07981785f4629c5ed1941107880da9574
Administrator <Administrator@UK0EG6KHE48.mshome.net>
2011-06-19 23:52:25 +0800

    normal update

---
+++ auth/model/staffWebAcess.php
@@ -0,0 +1,118 @@
+<?php require_once("../../class/classValidation.php");
+
+/**
+ * this is document model file.This is to ensure strict setting enable for all variable enter to database
+ *
+ * @name IDCMS.
+ * @version 2
+ * @author hafizan
+ * @package Document
+ * @link http://www.idcms.org
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
+ */
+class staffWebAcessModel extends validationClass{
+
+
+	private $staffWebAccessId;
+	private $staffId;
+	private $staffWebAccessLogIn;
+	private $staffWebAccessLogOut;
+
+	/**
+	 *   Class Loader to load outside variable and test it suppose variable type
+	 */
+	function execute(){
+		/*
+		 *  Basic Information Table
+		 */
+		$this->setTableName 		('staffWebAcess');
+		$this->setPrimaryKeyName 	('staffWebAcessId');
+
+		if($this->getVendor()==self::mysql){
+			$this->setStaffWebAccessLogIn("'".date("Y-m-d H:i:s")."'");
+		} else if ($this->getVendor()==self::mssql){
+			$this->setStaffWebAccessLogIn("'".date("Y-m-d H:i:s")."'");
+		} else if ($this->getVendor()==self::oracle){
+			$this->setStaffWebAccessLogIn("to_date('".date("Y-m-d H:i:s")."','YYYY-MM-DD HH24:MI:SS')");
+		} else{
+			echo "udentified vendor ?";
+		}
+		if($this->getVendor()==self::mysql){
+			$this->setStaffWebAccessLogin("'".date("Y-m-d H:i:s")."'");
+		} else if ($this->getVendor()==self::mssql){
+			$this->setStaffWebAccessLogOut("'".date("Y-m-d H:i:s")."'");
+		} else if ($this->getVendor()==self::oracle){
+			$this->setStaffWebAccessLogOut("to_date('".date("Y-m-d H:i:s")."','YYYY-MM-DD HH24:MI:SS')");
+		} else{
+			echo "udentified vendor ?";
+		}
+
+
+	}
+	/* (non-PHPdoc)
+	 * @see validationClass::create()
+	 */
+	public function create()
+	{
+
+	}
+	/* (non-PHPdoc)
+	 * @see validationClass::update()
+	 */
+	public function update()
+	{
+
+	}
+	/* (non-PHPdoc)
+	 * @see validationClass::delete()
+	 */
+	public function delete()
+	{
+
+	}
+
+	/**
+	 * Set Staff Web Access Identification Value
+	 * @param numeric $value
+	 */
+	public function setStaffWebAccessId($value) {
+		$this->staffWebAccessId = $value;
+	}
+	/**
+	 * Return Staff Web Access Identification Value
+	 * @return numeric Staff Web Access Value
+	 */
+	public function getStaffWebAccessId() {
+		return $this->staffWebAccessId;
+	}
+	/**
+	 * Set Staff Web Access Login Value
+	 * @param datetime $value
+	 */
+	public function setStaffWebAccessLogIn($value) {
+		$this->staffWebAccessLogIn = $value;
+	}
+	/**
+	 * Return Staff Web Access Login Value
+	 * @return numeric Document Cateogory Identification Value
+	 */
+	public function getStaffWebAccessLogIn() {
+		return $this->staffWebAccessLogIn;
+	}
+	/**
+	 * Set Staff Web Access LogOut Value
+	 * @param datetime $value
+	 */
+	public function setStaffWebAccessLogOut($value) {
+		$this->staffWebAccessLogOut = $value;
+	}
+	/**
+	 * Return Staff Web Access LogOut Value
+	 * @return datetime Staff Web Access LogOut
+	 */
+	public function getStaffWebAccessLogOut() {
+		return $this->staffWebAccessLogOut;
+	}
+
+}
+?>
---
+++ document/class/classDocumentTrail.php
@@ -0,0 +1,247 @@
+<?php
+/**
+ * Document Trail Audit.All Preprint Microsoft Excel Will Be Tracked
+ * @author hafizan
+ *
+ */
+class documentTrailClass extends configClass {
+	/*
+	 * Connection to the database
+	 * @var string $excel
+	 */
+	public $q;
+	/*
+	 *
+	 */
+	public $model;
+	private $leafId;
+	private $path;
+	private $filename;
+	/**
+	 *	Class Loader
+	 */
+	function execute() {
+		parent :: __construct();
+
+		$this->q 					=	new vendor();
+
+		$this->q->vendor			=	$this->getVendor();
+
+		$this->q->leafId			=	$this->getLeafId();
+
+		$this->q->staffId			=	$this->getStaffId();
+
+		$this->q->connect($this->getConnection(), $this->getUsername(), $this->getDatabase(), $this->getPassword());
+
+		$this->model = new documentModel();
+
+		$this->model->setVendor($this->getVendor());
+
+		$this->model->execute();
+
+
+
+	}
+	/* (non-PHPdoc)
+	 * @see config::create()
+	 */
+	public function create(){
+
+		$this->model->create();
+		/**
+		 *	Define basic audit trail system...
+		 * 	@params numeric $documentCategoryId = 3
+		 **/
+		$this->model->setDocumentCategoryId(3);
+		$this->model->setLeafId($this->getLeafId());
+		$this->model->setDocumentPath($this->getDocumentPath());
+		$this->model->setDocumentFilename($this->getDocumentFilename());
+
+
+
+
+		$this->q->start();
+
+		if($this->getVendor()==self::mysql){
+			$sql = "
+			INSERT INTO `document`	(
+						`documentCategoryId`,			`leafId`,
+						`documentTitle`,				`documentDesc`,
+						`documentPath`,					`documentFilename`,
+						`isDefault`,					`isNew`,
+						`isDraft`,						`isUpdate`,
+						`isDelete`,						`isActive`,
+						`isApproved`,					`By`,
+						`Time`
+			)	VALUES	(
+						\"".$this->model->getDocumentCategoryId()."\",
+						\"".$this->model->getLeafId()."\",
+						\"".$this->model->getDocumentTitle()."\",
+						\"".$this->model->getDocumentDesc()."\",
+						\"".$this->model->getDocumentPath()."\",
+						\"".$this->model->getDocumentFilename()."\",
+						\"".$this->model->getIsDefault('','string')."\",
+						\"".$this->model->getIsNew('','sring')."\",
+						\"".$this->model->getIsdraft('','string')."\",
+						\"".$this->model->getIsUpdate('','string')."\",
+						\"".$this->model->getIsDelete('','sring')."\",
+						\"".$this->model->getIsActive('','string')."\",
+						\"".$this->model->getIsApproved('','string')."\",
+						\"".$this->model->getBy()."\",
+						 ".$this->model->getTime()."
+			); ";
+		} else if ($this->getVendor()==self::mssql){
+			$sql = "
+			INSERT INTO [document]	(
+						[documentCategoryId],			[leafId],
+						[documentTitle],				[documentDesc],
+						[documentPath],					[documentFilename],
+						[isDefault],					[isNew],
+						[isDraft],						[isUpdate],
+						[isDelete],						[isActive],
+						[isApproved],					[By],
+						[Time]
+			)	VALUES	(
+						\"".$this->model->getDocumentCategoryId()."\",
+						\"".$this->model->getLeafId()."\",
+						\"".$this->model->getDocumentTitle()."\",
+						\"".$this->model->getDocumentDesc()."\",
+						\"".$this->model->getPath()."\",
+						\"".$this->model->getFilename()."\",
+						\"".$this->model->getIsDefault('','string')."\",
+						\"".$this->model->getIsNew('','sring')."\",
+						\"".$this->model->getIsdraft('','string')."\",
+						\"".$this->model->getIsUpdate('','string')."\",
+						\"".$this->model->getIsDelete('','sring')."\",
+						\"".$this->model->getIsActive('','string')."\",
+						\"".$this->model->getIsApproved('','string')."\",
+						\"".$this->model->getBy()."\",
+						 ".$this->model->getTime()."
+			); ";
+		} else if ($this->getVendor()==self::oracle){
+			$sql = "
+			INSERT INTO \"document`	(
+						\"documentCategoryId\",			\"leafId\",
+						\"documentTitle\",				\"documentDesc\",
+						\"documentPath\",				\"documentFilename\",
+						\"isDefault\",					\"isNew\",
+						\"isDraft\",					\"isUpdate\",
+						\"isDelete\",					\"isActive\",
+						\"isApproved\",					\"By\",
+						\"Time\"
+			)	VALUES	(
+						\"".$this->model->getDocumentCategoryId()."\",
+						\"".$this->model->getLeafId()."\",
+						\"".$this->model->getDocumentTitle()."\",
+						\"".$this->model->getDocumentDesc()."\",
+						\"".$this->model->getPath()."\",
+						\"".$this->model->getFilename()."\",
+						\"".$this->model->getIsDefault('','string')."\",
+						\"".$this->model->getIsNew('','sring')."\",
+						\"".$this->model->getIsdraft('','string')."\",
+						\"".$this->model->getIsUpdate('','string')."\",
+						\"".$this->model->getIsDelete('','sring')."\",
+						\"".$this->model->getIsActive('','string')."\",
+						\"".$this->model->getIsApproved('','string')."'\",
+						\"".$this->model->getBy()."\",
+						 ".$this->model->getTime()."
+			); ";
+		}
+		$this->q->create($sql);
+		$this->q->commit();
+
+		if($this->q->execute=='fail') {
+			echo json_encode(array("success"=>false,"message"=>$this->q->responce));
+			exit();
+		}
+	}
+	/* (non-PHPdoc)
+	 * @see config::read()
+	 */
+	public function read() {}
+	/* (non-PHPdoc)
+	 * @see config::update()
+	 */
+	public function update(){}
+	/* (non-PHPdoc)
+	 * @see config::delete()
+	 */
+	public function delete(){}
+	/* (non-PHPdoc)
+	 * @see config::excel()
+	 */
+	public function excel(){}
+	/**
+	 * File Information
+	 * @param string $filename
+	 * @return mixed
+	 */
+	public function fileExtension($filename)
+	{
+		$path_info = pathinfo($filename);
+		return $path_info['extension'];
+	}
+
+	/**
+	 * Remove File Extension
+	 * @param string $filename
+	 * @return mixed
+	 */
+	public function removeExtension($filename) {
+		return preg_replace('/(.+)\..*$/', '$1', $filename);
+	}
+
+	/**
+	 * Document Audit Trail
+	 * @param numeric $leafId
+	 * @param string $path
+	 * @param string $filename
+	 */
+	public function createTrail($leafId,$path,$filename){
+
+
+
+	}
+	/**
+	 * Set Leaf Identification Value
+	 * @param numeric $value
+	 */
+	public function setLeafId($value) {
+		$this->leafId = $value;
+	}
+	/**
+	 * Return Leaf Identification Value
+	 * @return numeric Document Cateogory Identification Value
+	 */
+	public function getLeafId() {
+		return $this->leafId;
+	}
+/**
+	 * Set Document Path Value
+	 * @param boolean $value
+	 */
+	public function setDocumentPath($value) {
+		$this->documentPath = $value;
+	}
+	/**
+	 * Return Document title
+	 * @return string document title
+	 */
+	public function getDocumentPath() {
+		return $this->documentPath;
+	}
+	/**
+	 * Set Document Filename Value
+	 * @param boolean $value
+	 */
+	public function setDocumentFilename($value) {
+		$this->documentFilename = $value;
+	}
+	/**
+	 * Return Document Filename
+	 * @return string document title
+	 */
+	public function getDocumentFilename() {
+		return $this->documentFilename;
+	}
+}
