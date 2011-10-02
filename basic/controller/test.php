3f03224303d774e29b91058f15ff40a50bbb078e
alien3d <hafizanil@hotmail.com>
2011-10-02 16:32:44 +0800

    adding folder jasper report

---
+++ basic/controller/test.php
@@ -0,0 +1,20 @@
+<?php
+class s{
+    private $x;
+	/**
+     * @return the $x
+     */
+    public function getX ()
+    {
+        return $this->x;
+    }
+
+	/**
+     * @param field_type $x
+     */
+    public function setX ($x)
+    {
+        $this->x = $x;
+    }
+
+}
---
+++ phpjasperxml_0.8/setting.php
@@ -0,0 +1,9 @@
+<?php
+$server="localhost";
+$db="phpjasperxml";
+$user="root";
+$pass="user1";
+$version="0.8";
+$pgport=5432;
+$pchartfolder="./class/pchart2";
+?>
