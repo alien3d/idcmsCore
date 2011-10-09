a4cd198969d24a7518629d4bf7faaf29d41bc2c8
alien3d <hafizanil@hotmail.com>
2011-10-09 15:21:15 +0800

    update database

---
+++ class/test.php
@@ -0,0 +1,41 @@
+<?php
+interface  testing {
+	function nama();
+	function umur();
+}
+class satu implements  testing{
+	public function nama(){
+		
+	}
+	public function umur(){
+		
+	}
+}
+class dua implements  testing{
+	public function nama(){
+		
+	}
+	public function umur() {
+		
+	}
+}
+class tiga implements testing{
+	/* (non-PHPdoc)
+	 * @see testing::nama()
+	 */
+	public function nama() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-PHPdoc)
+	 * @see testing::umur()
+	 */
+	public function umur() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
+?>
